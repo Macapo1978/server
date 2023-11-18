f2d4d98244fc907a9e1fd37d01ab4f0a16375c34
Mariana Capo <macapo78@gmail.com>
2023-11-18 10:55:30 -0500

    crud patients and endpoints

---
+++ routes/patient-routes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const patientController = require("../controllers/patient-controller");
+
+router
+    .route('/api/patients')
+    .post(patientController.createPatient)
+    .get(patientController.getAllPatients);
+
+router
+    .route('/api/patients/:id')
+    .get(patientController.getPatientById)
+    .put(patientController.updatePatient)
+    .delete(patientController.deletePatient);
+
+module.exports = router;
