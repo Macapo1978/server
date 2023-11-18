e355871a56bb73f1e27b371fbbdbfed75de08ed8
Mariana Capo <macapo78@gmail.com>
2023-11-18 12:46:30 -0500

    added rel patients audiology

--- controllers/patient-controller.js
+++ controllers/patient-controller.js
@@ -8,7 +8,7 @@ const getPatientById = async (req, res) => {
         .where('patients.id', patientId)
         .innerJoin('patients_audiologies', 'patients.id', 'patients_audiologies.patient_id')
         .innerJoin('audiologies', 'patients_audiologies.audiology_id', 'audiologies.id')
-        .select('patients.id', 'patients.name', 'patients.last_name', 'audiologies.id as audiology_id', 'audiologies.name as audiology_name', 'audiologies.last_name as audiology_last_name')
+        .select('patients.id', 'patients.name', 'patients.last_name', 'patients.user_id','audiologies.id as audiology_id', 'audiologies.name as audiology_name', 'audiologies.last_name as audiology_last_name')
         .first();
 
     if (!patient) {
@@ -24,10 +24,9 @@ const getPatientById = async (req, res) => {
 const getAllPatients = async (req, res) => {
   try {
     const patients = await knex('patients')
-        .where('patients.id', patientId)
         .innerJoin('patients_audiologies', 'patients.id', 'patients_audiologies.patient_id')
         .innerJoin('audiologies', 'patients_audiologies.audiology_id', 'audiologies.id')
-        .select('patients.id', 'patients.name', 'patients.last_name', 'audiologies.id as audiology_id', 'audiologies.name as audiology_name', 'audiologies.last_name as audiology_last_name');
+        .select('patients.id', 'patients.name', 'patients.last_name', 'patients.user_id', 'audiologies.id as audiology_id', 'audiologies.name as audiology_name', 'audiologies.last_name as audiology_last_name');
 
     res.status(200).json(patients);
   } catch (error) {
@@ -45,40 +44,46 @@ const valNotNull = ( inputPatient ) => {
 }
 
 const audiologyExists = async ( audiology_id) => {
-    const audiologyExists = await knex('audiologies').where('id', audiology_id).first();
+    const audiology = await knex('audiologies').where('id', audiology_id).first();
+    return !!audiology;
+}
 
-    if (!audiologyExists) {
-      return false;
-    }
-    return true;
+const userExists = async ( user_id ) => {
+    const user = await knex('users').where('id', user_id).first();
+    return !!user;
 }
 
 const createPatient = async (req, res) => {
     if (!valNotNull(req.body)) {
         return res.status(400).json({ error: 'All fields are required.' });
     }
-    if (!audiologyExists (req.body.audiology_id)) {
-      return res.status(404).json({ error: 'Audiology not found.' });
-    }
-
     const { name, last_name, user_id, audiology_id } = req.body;
+    
+    const foundAudiology = await audiologyExists(audiology_id);
+    if (!foundAudiology) {
+      return res.status(404).json({ error: `Audiology id ${audiology_id} not found.` });
+    }
+    const foundUser = await userExists(user_id);
+    if (!foundUser) {
+        return res.status(404).json({ error: `User id ${user_id} not found.` });
+    }
+    const trx = await knex.transaction();
+
     try {
-        await knex.transaction(async (trx) => {
-            const [patientId] = await trx('patients').insert({ name, last_name, user_id });
-            await trx('patients_audiologies').insert({ patient_id: patientId, audiology_id});
+        const [patientId] = await trx('patients').insert({ name, last_name, user_id });
+        await trx('patients_audiologies').insert({ patient_id: patientId, audiology_id});
 
-            await trx.commit();
+        await trx.commit();
 
-            const newPatient = await knex('patients')
-                .where('patients.id', patientId)
-                .innerJoin('patients_audiologies', 'patients.id', 'patients_audiologies.patient_id')
-                .innerJoin('audiologies', 'patients_audiologies.audiology_id', 'audiologies.id')
-                .select('patients.id', 'patients.name', 'patients.last_name', 'audiologies.id as audiology_id', 'audiologies.name as audiology_name', 'audiologies.last_name as audiology_last_name')
-                .first();
+        const newPatient = await knex('patients')
+            .where('patients.id', patientId)
+            .innerJoin('patients_audiologies', 'patients.id', 'patients_audiologies.patient_id')
+            .innerJoin('audiologies', 'patients_audiologies.audiology_id', 'audiologies.id')
+            .select('patients.id', 'patients.name', 'patients.last_name', 'audiologies.id as audiology_id', 'audiologies.name as audiology_name', 'audiologies.last_name as audiology_last_name')
+            .first();
 
-            res.status(201).json(newPatient);
+        res.status(201).json(newPatient);
         
-        });
     } catch (error) {
         await trx.rollback();
         res.status(500).json({ error: `Error creating patient: ${error}` });
@@ -89,33 +94,37 @@ const updatePatient = async (req, res) => {
     if (!valNotNull(req.body)) {
         return res.status(400).json({ error: 'All fields are required.' });
     }
-    if (!audiologyExists (req.body.audiology_id)) {
-        return res.status(404).json({ error: 'Audiology not found.' });
-    }
-    
-    const patientId = req.params.id;
     const { name, last_name, user_id, audiology_id } = req.body;
 
+    const foundAudiology = await audiologyExists(audiology_id);
+    if (!foundAudiology) {
+      return res.status(404).json({ error: `Audiology id ${audiology_id} not found.` });
+    }
+    const foundUser = await userExists(user_id);
+    if (!foundUser) {
+        return res.status(404).json({ error: `User id ${user_id} not found.` });
+    }
+
+    const patientId = req.params.id;
+    const trx = await knex.transaction();
     try {
-        await knex.transaction( async (txt) => { 
-            const updatedRows = await trx('patients').where('id', patientId).update({ name, last_name, user_id });
-            if (updatedRows === 0) {
-                trx.rollback();
-                return res.status(404).json({ message: `Patient with ID ${patientId} not found.` });
-            }
-            await trx('patients_audiologies').where('patient_id', patientId).update({audiology_id});
-            trx.commit();
+        const updatedRows = await trx('patients').where('id', patientId).update({ name, last_name, user_id });
+        if (updatedRows === 0) {
+            trx.rollback();
+            return res.status(404).json({ message: `Patient with ID ${patientId} not found.` });
+        }
+        await trx('patients_audiologies').where('patient_id', patientId).update({audiology_id});
+        trx.commit();
 
-            const updatedPatient = await knex('patients')
-                .where('patients.id', patientId)
-                .innerJoin('patients_audiologies', 'patients.id', 'patients_audiologies.patient_id')
-                .innerJoin('audiologies', 'patients_audiologies.audiology_id', 'audiologies.id')
-                .select('patients.id', 'patients.name', 'patients.last_name', 'audiologies.id as audiology_id', 'audiologies.name as audiology_name', 'audiologies.last_name as audiology_last_name')
-                .first();
+        const updatedPatient = await knex('patients')
+            .where('patients.id', patientId)
+            .innerJoin('patients_audiologies', 'patients.id', 'patients_audiologies.patient_id')
+            .innerJoin('audiologies', 'patients_audiologies.audiology_id', 'audiologies.id')
+            .select('patients.id', 'patients.name', 'patients.last_name', 'patients.user_id','audiologies.id as audiology_id', 'audiologies.name as audiology_name', 'audiologies.last_name as audiology_last_name')
+            .first();
 
-            res.status(200).json(updatedPatient);
+        res.status(200).json(updatedPatient);
 
-        });
     } catch (error) {
         await trx.rollback();
         res.status(500).json({ error: `Error updating patient: ${patientId} error: ${error}` });
