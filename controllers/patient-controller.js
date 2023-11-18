2024febfe853f1f2eaeab8cd63520527dc62f8b2
Mariana Capo <macapo78@gmail.com>
2023-11-18 11:35:55 -0500

    changed createPatient to add audiology_id

--- controllers/patient-controller.js
+++ controllers/patient-controller.js
@@ -28,15 +28,38 @@ const getAllPatients = async (req, res) => {
 };
 
 const createPatient = async (req, res) => {
-  const { name, last_name, user_id } = req.body;
+    const { name, last_name, user_id, audiology_id } = req.body;
 
-  try {
-    const [patientId] = await knex('patients').insert({ name, last_name, user_id });
-    const newPatient = await knex('patients').where('id', patientId).first();
-    res.status(201).json(newPatient);
-  } catch (error) {
-    res.status(500).json({ error: `Error creating patient: ${error}` });
-  }
+    if (!name || !last_name || !user_id || !audiology_id) {
+        return res.status(400).json({ error: 'All fields are required.' });
+    }
+    const audiologyExists = await knex('audiologies').where('id', audiology_id).first();
+
+    if (!audiologyExists) {
+      return res.status(404).json({ error: 'Audiology not found.' });
+    }
+
+    try {
+        await knex.transaction(async (trx) => {
+            const [patientId] = await trx('patients').insert({ name, last_name, user_id });
+            await trx('patients_audiologies').insert({ patient_id: patientId, audiology_id});
+
+            await trx.commit();
+
+            const newPatient = await knex('patients')
+                .where('patients.id', patientId)
+                .innerJoin('patients_audiologies', 'patients.id', 'patients_audiologies.patient_id')
+                .innerJoin('audiologies', 'patients_audiologies.audiology_id', 'audiologies.id')
+                .select('patients.id', 'patients.name', 'patients.last_name', 'audiologies.id as audiology_id', 'audiologies.name as audiology_name', 'audiologies.last_name as audiology_last_name')
+                .first();
+
+            res.status(201).json(newPatient);
+        
+        });
+    } catch (error) {
+        await trx.rollback();
+        res.status(500).json({ error: `Error creating patient: ${error}` });
+    }
 };
 
 const updatePatient = async (req, res) => {
